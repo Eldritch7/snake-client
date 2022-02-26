033e9fcdcc55775a883b8ec65e6adde4d32dc75a
Sara <parakeet_22@yahoo.com>
2022-02-26 18:39:32 -0500

    in play the connect function is put into the setupInput function, which can now receive the conn object and set a playholder connection variable to conn and be used to write movement commands to the server

--- input.js
+++ input.js
@@ -1,24 +1,42 @@
+const { connect } = require("http2");
 const { stdin } = require("process");
+let connection;
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
+  //console.log(conn);
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  //stdin.on("data", handleUserInput);
   return stdin;
 };
 
-const handleUserInput = function(key) {
+
+
+const handleUserInput = process.stdin.on('data', (key) => {
 
 
   if (key === '\u0003') {
     process.exit();
+  } else if (key === "w" || key === "W") {
+    //console.log("Move up!");
+    connection.write("Move: up");
+  } else if (key === "s" || key === "S") {
+    //console.log("Move down!");
+    connection.write("Move: down");
+  } else if (key === "a" || key === "A") {
+    //console.log("Move left!");
+    connection.write("Move: left");
+  } else if (key === "d" || key === "D") {
+    //console.log("Move right!");
+    connection.write("Move: right");
   }
   
  
   
-};
+});
 
 module.exports = {
 setupInput
--- play.js
+++ play.js
@@ -1,10 +1,10 @@
 //This will serve as our main file
 // through which we will launch the game client
-const { stdin } = require("process");
+
 const {connect} = require("./client");
 const {setupInput} = require("./input");
 
 
 console.log("Connecting ...");
-connect();
-setupInput();
+
+setupInput(connect());
