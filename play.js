d89365988525f7a629557ca6b0f3d30400b4889f
Sara <parakeet_22@yahoo.com>
2022-02-26 17:31:52 -0500

    set up a setupInput function with stdin and made a callback function that knows to exit the program if ctrl c is pressed setUpInput is called at the end

--- play.js
+++ play.js
@@ -1,7 +1,31 @@
 //This will serve as our main file
 // through which we will launch the game client
+const { stdin } = require("process");
 const {net, connect} = require("./client");
 
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+ 
+  
+};
+setupInput();
